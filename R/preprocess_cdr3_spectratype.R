08ed067c9e583dd270b0c475b095f34ffcd2c897
mcordes <m.cordes@lumc.nl>
2019-07-29 23:38:38 +0200

    settings in preprocessing

--- R/preprocess_cdr3_spectratype.R
+++ R/preprocess_cdr3_spectratype.R
@@ -20,6 +20,7 @@ preprocess_cdr3_spectratype <- function (folder)
   }
 
   #Preprocess
+  fsa.loaded <- storing.inds(folder)
   my.ladder <- c(35, 50, 75, 100, 139, 150, 160, 200, 250,300, 340, 350, 400, 450, 490, 500)
   ladder.info.attach(stored=fsa.loaded, ladder=my.ladder, ladd.init.thresh=1000, draw=F,method="iter2")
   spectratype_dataset <- get_basepair_positions(fsa.loaded, cols = 1, my.ladder, channel.ladder=NULL,  init.thresh=1750, ladd.init.thresh=1000)
