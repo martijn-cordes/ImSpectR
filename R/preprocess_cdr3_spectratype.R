2523e2f1d1fcedae30f5339ca3b95f7ed83f90c5
mcordes <m.cordes@lumc.nl>
2019-09-04 16:10:52 +0200

    fixed ladder problems + peak window bug

--- R/preprocess_cdr3_spectratype.R
+++ R/preprocess_cdr3_spectratype.R
@@ -22,8 +22,27 @@ preprocess_cdr3_spectratype <- function (folder)
   #Preprocess
   fsa.loaded <- storing.inds(folder)
   my.ladder <- c(35, 50, 75, 100, 139, 150, 160, 200, 250,300, 340, 350, 400, 450, 490, 500)
-  ladder.info.attach(stored=fsa.loaded, ladder=my.ladder, ladd.init.thresh=1000, draw=F,method="iter2")
-  spectratype_dataset <- get_basepair_positions(fsa.loaded, cols = 1, my.ladder, channel.ladder=NULL,  init.thresh=1750, ladd.init.thresh=1000)
+
+  for (i in 1:length(fsa.loaded)) {
+
+    fsa.loaded[[i]][,1][which(fsa.loaded[[i]][,1] < 0)] <- 0
+    fsa.loaded[[i]][,2][which(fsa.loaded[[i]][,2] < 0)] <- 0
+    fsa.loaded[[i]][,4][which(fsa.loaded[[i]][,4] < 0)] <- 0
+
+
+    left <- which(fsa.loaded[[i]][,4] > (max(fsa.loaded[[i]][,4][3000:length(fsa.loaded[[i]][,4])])+100)) - 10
+    right <- which(fsa.loaded[[i]][,4] > (max(fsa.loaded[[i]][,4][3000:length(fsa.loaded[[i]][,4])])+100)) + 10
+    for(d in 1:length(left)) {
+
+      fsa.loaded[[i]][,4][left[d]:right[d]] <- 0
+
+    }
+
+  }
+
+
+  ladder.info.attach(stored=fsa.loaded, ladder=my.ladder, ladd.init.thresh=300, draw=F,method="iter2")
+  spectratype_dataset <- get_basepair_positions(fsa.loaded, cols = 1, my.ladder, channel.ladder=NULL,  init.thresh=1750, ladd.init.thresh=300)
 
   spectratype_dataset
 
